43a2ad6442f4b53f5bafaf7bfdef65685e7328e5
Josh Haglund <jhaglund@camsys.com>
2016-11-29 12:00:42 -0800

    Added 'invalidate' option to deploy scripts to invalidate the cloudfront distribution after deploying to s3. use:  deploy.dev.sh invalidate

--- deploy.dev.sh
+++ deploy.dev.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 aws s3 sync ./dist/ s3://1click-dev.camsys-apps.com/ --acl public-read
 
+if [ "$1" == "invalidate" ]
+then
+aws configure set preview.cloudfront true
+aws cloudfront create-invalidation \
+    --distribution-id E1EWGNVO71RU9C \
+    --paths ./
+fi
+
