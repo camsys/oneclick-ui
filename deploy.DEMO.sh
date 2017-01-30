7f1e82c93f651c4eec4e664fe5e1a1124ad8467c
Josh Haglund <jhaglund@camsys.com>
2017-01-30 11:11:06 -0800

    invalidate by default

--- deploy.DEMO.sh
+++ deploy.DEMO.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 aws s3 sync ./dist/ s3://1click-demo.camsys-apps.com/ --acl public-read --region us-west-2
 
-if [ "$1" == "invalidate" ]
-then
 aws configure set preview.cloudfront true
 aws cloudfront create-invalidation \
     --distribution-id E2Q6A8WGOUD62Y \
     --paths '/*'
-fi
 
--- deploy.qa.sh
+++ deploy.qa.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 aws s3 sync ./dist/ s3://1click-qa.camsys-apps.com/ --acl public-read
 
-if [ "$1" == "invalidate" ]
-then
 aws configure set preview.cloudfront true
 aws cloudfront create-invalidation \
     --distribution-id E107CYV4E4P5UE \
     --paths '/*'
-fi
 
