a78acbd4894b4e649fa7de3c0c9e314e0e336919
Josh Haglund <jhaglund@camsys.com>
2017-02-07 14:12:40 -0800

    use https for updating translations, api change

--- update_dist_translations.sh
+++ update_dist_translations.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-curl http://oneclick-uta-qa.herokuapp.com/api/v1/translations/all?lang=en > ./dist/translations/en.json
-curl http://oneclick-uta-qa.herokuapp.com/api/v1/translations/all?lang=es > ./dist/translations/es.json
+curl https://oneclick-uta-qa.herokuapp.com/api/v1/translations/all?lang=en > ./dist/translations/en.json
+curl https://oneclick-uta-qa.herokuapp.com/api/v1/translations/all?lang=es > ./dist/translations/es.json
 rm ./app/translations/*.json
 cp ./dist/translations/*.json ./app/translations/
 
