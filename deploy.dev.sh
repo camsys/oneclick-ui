78085bfe3eb983bdbb8fab7083465826eb7dc425
Josh Haglund <jhaglund@camsys.com>
2017-02-09 16:51:25 -0800

    update translations before deploy

--- deploy.dev.sh
+++ deploy.dev.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+curl https://oneclick-uta-qa.herokuapp.com/api/v1/translations/all?lang=en > ./dist/translations/en.json
+curl https://oneclick-uta-qa.herokuapp.com/api/v1/translations/all?lang=es > ./dist/translations/es.json
+
 aws s3 sync ./dist/ s3://1click-dev.camsys-apps.com/ --acl public-read
 
 aws configure set preview.cloudfront true
--- deploy.qa.sh
+++ deploy.qa.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+curl https://oneclick-uta-qa.herokuapp.com/api/v1/translations/all?lang=en > ./dist/translations/en.json
+curl https://oneclick-uta-qa.herokuapp.com/api/v1/translations/all?lang=es > ./dist/translations/es.json
+
 aws s3 sync ./dist/ s3://1click-qa.camsys-apps.com/ --acl public-read
 
 aws configure set preview.cloudfront true
