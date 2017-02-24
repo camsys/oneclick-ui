e62cece6a8bdac6fc23a2c63e541dd5005a0ca6c
Josh Haglund <jhaglund@camsys.com>
2017-02-24 08:59:19 -0800

    point QA translations to PA-QA, not UTA-QA

--- deploy.qa.sh
+++ deploy.qa.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-curl https://oneclick-uta-qa.herokuapp.com/api/v1/translations/all?lang=en > ./dist/translations/en.json
-curl https://oneclick-uta-qa.herokuapp.com/api/v1/translations/all?lang=es > ./dist/translations/es.json
+curl https://oneclick-pa-qa.herokuapp.com/api/v1/translations/all?lang=en > ./dist/translations/en.json
+curl https://oneclick-pa-qa.herokuapp.com/api/v1/translations/all?lang=es > ./dist/translations/es.json
 
 aws s3 sync ./dist/ s3://1click-qa.camsys-apps.com/ --acl public-read
 
