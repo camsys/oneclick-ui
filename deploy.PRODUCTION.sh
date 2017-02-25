778c8cb70a114643ede4cfc1ce26d94fcd8ace9d
Josh Haglund <jhaglund@camsys.com>
2017-02-24 17:23:17 -0800

    removing UTA things that are in UTA repo now

--- deploy.PRODUCTION.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-aws s3 sync ./dist/ s3://1click-uta-prod.camsys-apps.com/ --acl public-read
-
-aws configure set preview.cloudfront true
-aws cloudfront create-invalidation \
-    --distribution-id E1O2FWVP0S6LVW \
-    --paths '/*'
-
