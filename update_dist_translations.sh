53db17dc7a2b94dbb94cda6096d818e403c13aaf
Josh Haglund <jhaglund@camsys.com>
2017-03-02 12:59:50 -0800

    removing this script because translations are updated by grunt now

--- update_dist_translations.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-curl https://oneclick-uta-qa.herokuapp.com/api/v1/translations/all?lang=en > ./dist/translations/en.json
-curl https://oneclick-uta-qa.herokuapp.com/api/v1/translations/all?lang=es > ./dist/translations/es.json
-rm ./app/translations/*.json
-cp ./dist/translations/*.json ./app/translations/
-
