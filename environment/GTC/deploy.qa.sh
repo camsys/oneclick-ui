533c634bc7b653e21d91422e0e0d62903700d541
Josh Haglund <jhaglund@camsys.com>
2017-03-23 17:48:23 -0700

    re-arranging deploy scripts, each environment now contains its own scripts

--- environment/GTC/deploy.qa.sh
+++ environment/GTC/deploy.qa.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-curl https://oneclick-uta-qa.herokuapp.com/api/v1/translations/all?lang=en > ./dist/translations/en.json
-curl https://oneclick-uta-qa.herokuapp.com/api/v1/translations/all?lang=es > ./dist/translations/es.json
 
+## Deploy script for 1click environment
+
+## sync the ./dist directory with the s3 bucket
 aws s3 sync ./dist/ s3://1click-qa.camsys-apps.com/ --acl public-read
 
+## Invalidate the cloudfront distribution
 aws configure set preview.cloudfront true
 aws cloudfront create-invalidation \
     --distribution-id E107CYV4E4P5UE \
---
+++ environment/UTA/deploy.PRODUCTION.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+## Deploy script for 1click environment
+
+## sync the ./dist directory with the s3 bucket
+aws s3 sync ./dist/ s3://1click-uta-prod.camsys-apps.com/ --acl public-read
+
+## Invalidate the cloudfront distribution
+aws configure set preview.cloudfront true
+aws cloudfront create-invalidation \
+    --distribution-id E1O2FWVP0S6LVW \
+    --paths '/*'
+
