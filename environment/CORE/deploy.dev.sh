1f9493562236ad8e9fdbaf4a827e3986fa3e6c32
Josh Haglund <jhaglund@camsys.com>
2017-03-31 10:16:12 -0700

    fix deploy script for CORE environment

--- environment/CORE/deploy.dev.sh
+++ environment/CORE/deploy.dev.sh
@@ -3,7 +3,7 @@
 ## Deploy script for 1click environment
 
 ## sync the ./dist directory with the s3 bucket
-aws s3 sync ./dist/ s3://1click-core-dev.camsys-apps.com/ --acl public-read
+aws s3 sync ./dist/ s3://1click-core-dev.camsys-apps.com/ --acl public-read  --region us-west-2
 
 ## Invalidate the cloudfront distribution
 aws configure set preview.cloudfront true
