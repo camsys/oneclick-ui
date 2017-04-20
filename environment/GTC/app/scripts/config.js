eb21ffe68fc30ca2b405e8846a4e23650b6b9aac
Josh Haglund <jhaglund@camsys.com>
2017-04-20 10:55:00 -0700

    expanded bounds, set state_bounds to NY to filter results

--- environment/GTC/app/scripts/config.js
+++ environment/GTC/app/scripts/config.js
@@ -1,11 +1,14 @@
 'use strict'
 
 var dist_env = {
+  // lower left: latA, lonA
+  // top right: latB, lonB
   map_bounds:{
-    latA: 42.478568,
-    lonA: -78.067808,
-    latB: 42.992985,
-    lonB: -77.471113
-  }
+    latA: 42.003898,
+    lonA: -78.313559,
+    latB: 43.457979,
+    lonB: -76.534205
+  },
+  state_bounds:'NY'
 };
 
