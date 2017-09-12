ecababad59e1e3cd37747152c0acece4e6742a30
Derek Edwards <dedwards8@gmail.com>
2017-09-11 22:06:57 -0400

    Add UTAOCC QA Env

--- environment/UTAOCC/deploy.qa.sh
+++ environment/UTAOCC/deploy.qa.sh
@@ -3,11 +3,11 @@
 ## Deploy script for 1click environment
 
 ## sync the ./dist directory with the s3 bucket
-aws s3 sync ./dist/ s3://1click-qa.camsys-apps.com/ --acl public-read
+aws s3 sync ./dist/ s3://occ-ui-uta-qa --acl public-read  --region us-east-2 --profile uta
 
 ## Invalidate the cloudfront distribution
-aws configure set preview.cloudfront true
-aws cloudfront create-invalidation \
-    --distribution-id E107CYV4E4P5UE \
-    --paths '/*'
+##aws configure set preview.cloudfront true
+##aws cloudfront create-invalidation \
+##    --distribution-id E1O2FWVP0S6LVW \
+##    --paths '/*'
 
