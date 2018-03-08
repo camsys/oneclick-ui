3534e6dae0349efe1db1ce253ffdcdc14927cf2e
Derek Edwards <dedwards8@gmail.com>
2018-03-08 15:24:38 -0500

    TEMP

---
+++ environment/GTCOCC/app/scripts/config.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var dist_env = {
+  // lower left: latA, lonA
+  // top right: latB, lonB
+  map_bounds:{
+    latA: 42.003898,
+    lonA: -78.313559,
+    latB: 43.457979,
+    lonB: -76.534205
+  },
+//  map_radius:{
+//    location:[42.594468, -77.474246],
+//    radius:60000
+//  },
+  state_bounds:'NY'
+};
+
