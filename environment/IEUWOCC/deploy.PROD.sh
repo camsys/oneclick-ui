3afe7081a8090db1357cb34b19586f6eca76e20d
tmahtab <tmahtab@camsys.com>
2018-04-06 11:27:05 -0700

    Added IEUWOCC environment

---
+++ environment/IEUWOCC/deploy.PROD.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+## Deploy script for 1click environment
+
+## sync the ./dist directory with the s3 bucket
+aws s3 sync ./dist/ s3://occ-ieuw-prod --acl public-read  --region us-west-1 --profile ieuw
+
+## Invalidate the cloudfront distribution
+#aws configure set preview.cloudfront true
+#aws cloudfront create-invalidation \
+#    --distribution-id E1O2FWVP0S6LVW \
+#    --paths '/*'
+
