fa64876167c538432b30dfd82628f884262b1f73
Derek Edwards <dedwards8@gmail.com>
2018-04-25 15:22:51 -0400

    Add carousel startup.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,5 +3,9 @@ angular.module('oneClickApp').controller('MainController', [
   '$scope',
   function ($scope) {
     $scope.foo = false;
+    $( document ).ready(function() {
+      $('.carousel').carousel();
+    });
+    
   }
 ]);
