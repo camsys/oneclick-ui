2b322cd223213f63ede62ecc5b4e08ae46e642bf
Tazeen Mahtab <tmahtab@camsys.com>
2018-04-27 16:07:31 -0700

    Added IEUW OCC 211 environment.

---
+++ environment/IEUWOCC211/app/scripts/config.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var dist_env = {
+  map_bounds:{
+    latA: 36.543524, 
+    lonA: -121.920082,
+    latB: 33.108948, 
+    lonB: -114.248192
+  },
+  transitSortPriority: true
+};
+
