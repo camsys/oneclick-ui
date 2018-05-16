1a5fbb6516560a48cdd9ed01d6786aed5e0a158a
Derek Edwards <dedwards8@gmail.com>
2018-05-16 15:50:33 -0400

    Update configs for JTA

--- environment/JTAOCC/deploy.QA.sh
+++ environment/JTAOCC/deploy.QA.sh
@@ -3,11 +3,12 @@
 ## Deploy script for 1click environment
 
 ## sync the ./dist directory with the s3 bucket
-aws s3 sync ./dist/ s3://1click-qa.camsys-apps.com/ --acl public-read
+#aws s3 sync ./dist/ s3://1click-qa.camsys-apps.com/ --acl public-read
+aws s3 sync ./dist/ s3://ui-jta-prod --acl public-read  --region us-east-1 --profile jta
 
 ## Invalidate the cloudfront distribution
-aws configure set preview.cloudfront true
-aws cloudfront create-invalidation \
-    --distribution-id E107CYV4E4P5UE \
-    --paths '/*'
+#aws configure set preview.cloudfront true
+#aws cloudfront create-invalidation \
+#    --distribution-id E107CYV4E4P5UE \
+#    --paths '/*'
 
